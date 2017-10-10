57d74530e239d9dcc91d1cceb073946d3051bf5c
jonas <jonasdthor@gmail.com>
2017-10-10 13:23:41 +0000

    Heimaverkefni 3 - Vfor 5

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Thread;
@@ -30,6 +31,18 @@ class ThreadsController extends Controller
     public function store(Request $request)
     {
       // Validate
+     
+     $validator = Validator::make($request->all(), [
+        'title' => 'required|max:40',
+        'body' => 'required|min:20',
+      ]);
+
+      if ($validator->fails()) {
+        return redirect('threads/create')
+          ->withInput()
+          ->withErrors($validator);
+      }
+
       $thread = new Thread;
       $thread->title = $request->title;
       $thread->body = $request->body;
--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -8,15 +8,16 @@
                 <div class="panel-heading">Create a New Thread</div>
 
                 <div class="panel-body">
+                  @include('common.errors')
                   <form method="post" action="/threads">
                     {{ csrf_field() }}
                     <div class="form-group">
                       <label>Title:</label>
-                      <input type="text" name="title" class="form-control">
+                      <input type="text" name="title" class="form-control" value="{{ old('title') }}" >
                     </div>
                     <div class="form-group">
                       <label>Body:</label>
-                      <textarea type="text" name="body" rows="7" class="form-control"></textarea>
+                      <textarea type="text" name="body" rows="7" class="form-control">{{{ Input::old('body') }}}</textarea>
                       <h1>  </h1>
                       <button class="btn btn-info" style="background-color:#5bc0de; border-color:#46B8D9">Publish</button>
                     </div>
